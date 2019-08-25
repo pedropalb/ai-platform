b6576cb6b7cb264e7beada718cee76888f696570
Pedro A. L. Barbosa <pedro17lopes@gmail.com>
2019-08-25 14:30:51 -0300

    Finish generate_tf_record.py script

--- tasks/computer-vision/image-orientation-correction/scripts/tf_record_generator.py
+++ tasks/computer-vision/image-orientation-correction/scripts/tf_record_generator.py
@@ -1,13 +1,40 @@
 from pathlib import Path
-import cv2
+
+import click
+import tensorflow as tf
 
 
-def generate(input_dir, output_dir, output_filename, index_path):
+def generate_example(jpeg_string, label):
+    jpeg_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[jpeg_string]))
+    label_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))
+
+    example = tf.train.Example(features=tf.train.Features(
+        feature={
+            'encoded': jpeg_feature,
+            'angle': label_feature
+        }
+    ))
+
+    return example.SerializeToString()
+
+
+@click.command()
+@click.option("--input-dir", required=True, help="The root dir containing the images.")
+@click.option("--output-path", default="data.tfrecord", show_default=True, help="The TFRecord file path.")
+def generate_tf_record(input_dir, output_path):
     input_dir = Path(input_dir).resolve()
+    output_path = Path(output_path).resolve()
 
-    with open(index_path, 'r') as f:
-        for line in f:
-            filename = line.strip()
-            file_path = input_dir / filename
+    with tf.io.TFRecordWriter(str(output_path)) as writer:
+        for file_path in input_dir.iterdir():
+            with open(str(file_path), 'rb') as f:
+                jpeg_string = f.read()
+                label = int(file_path.name.split('_')[0])
 
-            image = cv2.imread(str(file_path))
+                serialized_example = generate_example(jpeg_string, label)
+
+                writer.write(serialized_example)
+
+
+if __name__ == '__main__':
+    generate_tf_record()
